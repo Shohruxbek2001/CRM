dbb469a3ac635191eb282b60dc32ece5876f177d
OG'ABEK <shomoyxhacker007@mail.ru>
2022-04-22 10:16:18 +0500

    some info added

--- README.md
+++ README.md
@@ -103,3 +103,17 @@ npm run dev
 ```
 http://localhost:3000
 ```
+
+### Generate Access Token
+---
+Open the terminal then enter following command
+```
+node
+```
+```
+require('crypto').randomBytes(64).toString('hex')
+```
+show like this result
+```
+'6b6f21a64aa3ce9776e020beb670a2582749a35f751426df4e1b48e798c98b56646d18436e3c7d2e63812e2d6743bb21d1260a2550f599f46084c403fcc4a955'
+```
--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -4,7 +4,7 @@
       <div class="form-wrapper xl:w-4/12 lg:w-5/12 md:w-6/12 bg-slate-900/50 p-10 rounded-xl backdrop-blur-sm">
         <div id="clock" class="text-9xl text-white text-center font-medium">00:00</div>
         <div id="fullDate" class="text-4xl text-white text-center font-medium my-5 mb-10 uppercase">weekday 00 Month 0000</div>
-        <form @submit.prevent="onSubmit" :validation-schema="schema">
+        <Form @submit="onSubmit" :validation-schema="schema">
           <div class="mb-6">
             <!-- <input
               type="email"
@@ -21,7 +21,7 @@
               v-model="email"
               class="bg-white/25 border border-white text-white text-xl placeholder:text-white outline-none rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-4"
             />
-            <ErrorMessage name="email" class="error-feedback" />
+            <ErrorMessage name="email" class="error-feedback text-red-600 font-medium" />
           </div>
           <div class="mb-6">
             <!-- <input
@@ -39,12 +39,13 @@
               v-model="password"
               class="bg-white/25 border border-white text-white text-xl placeholder:text-white outline-none rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-4"
             />
-            <ErrorMessage name="password" class="error-feedback" />
+            <ErrorMessage name="password" class="error-feedback text-red-600 font-medium" />
           </div>
           <button type="submit" class="text-white bg-blue-700/75 hover:bg-blue-800/100 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-lg w-full px-5 py-4 text-center">
-            <svg v-show="isLoading" class="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24"></svg> Submit
+             Submit
           </button>
-        </form>
+          <svg class="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24"></svg>
+        </Form>
       </div>
     </div>
   </div>
@@ -54,6 +55,7 @@
 import { onMounted, ref } from 'vue'
 import axios from 'axios'
 import $ from 'jquery'
+import store from '../store/index'
 import { Form, Field, ErrorMessage } from 'vee-validate'
 import { useRouter } from 'vue-router'
 import * as yup from 'yup'
@@ -83,22 +85,23 @@ const password = ref('')
 let isLoading = false
 
 const schema = yup.object().shape({
-  username: yup.string().required('Iltimos. Emailni kitiring!'),
+  email: yup.string().required('Iltimos. Emailni kitiring!'),
   password: yup.string().required('Iltimos. Parolni kitiring!'),
 })
 
 const onSubmit = () => {
   isLoading = true
-  console.log(isLoading);
-  store.dispatch('api/login', user).then(
-    () => {
-      router.push('/')
-    },
-    (error) => {
-      isLoading = false
-      message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
-    }
-  )
+  console.log(isLoading)
+  // store.dispatch('api/login', user).then(
+  //   () => {
+  //     router.push('/')
+  //   },
+  //   (error) => {
+  //     isLoading = false
+  //     message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+  //   }
+  // )
+  // router.push('/')
 }
 //   axios
 //     .post('http://localhost:8000/login', {
